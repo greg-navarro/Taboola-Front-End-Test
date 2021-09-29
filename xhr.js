ebbb004ef8138ba2df41f7d85283ebb771bc9d98
greg-navarro <gregorylitmannavarro@gmail.com>
2021-09-28 17:11:20 -0700

    rewrite map frequencies

--- xhr.js
+++ xhr.js
@@ -21,43 +21,52 @@ for (el of words) {
 let moreCommonWords = ["are", "is", "where", "was"]
 wordList.push(...moreCommonWords)
 
+/* DEFINE FUNCTIONS */
+
 // define function to check if a word is valid with respects to requirements
 const isValidWord = (wordIn, commonWordList) => {
     let valid = true;
-    if (wordIn.length < 2 || wordIn in commonWordList) {
-        valid = false 
-    } else if (Number.isInteger(wordIn)) {
-        // check if word contains a digit or contains punctuation with regex TODO
-        // https://stackoverflow.com/questions/13925454/check-if-string-is-a-punctuation-character
-        valid = false
-    }
+    // if (wordIn.length < 2 || wordIn in commonWordList) {
+    //     valid = false 
+    // } else if (Number.isInteger(wordIn)) {
+    //     // check if word contains a digit or contains punctuation with regex TODO
+    //     // https://stackoverflow.com/questions/13925454/check-if-string-is-a-punctuation-character
+    //     valid = false
+    // }
     return true
 } 
 
 
 // define function that will map frequencies to words encountered on the page
-const mapFrequencies = (DOMelement, freqCounter, commonWords) => {
-    if ('innerText' in DOMelement) {
-        const textContent = DOMelement.innerText.split(' ')
-        console.log("got text")
-        for (word of textContent) {
-            if (isValidWord(word, commonWords)) {
-                console.log("valid word")
-                if (Object.keys(word)[0] in freqCounter) {
-                    freqCounter.word = freqCounter.word + 1
-                } else {
-                    freqCounter.word = 1
-                }
-            }
-        }
-    } 
-    
-    // recurse to child elements
-    for (child of DOMelement.children) {
-        // freqCounter = mapFrequencies(child, freqCounter)
-        console.log("OMG WE FOUND A CHILD")
-    }
+const mapFrequencies = (commonWords) => {
+    let freqCounter = {}
+    let fullText = $('body').text()
+    // replace all whitespace characters
+    const whiteSpace = /(\n|\t)/ig
+    fullText.replaceAll(whiteSpace, " ")
+    // replace all parenthesis, strip periods and commas
+    fullText.replaceAll(".", " ")
+    fullText.replaceAll(",", " ")
+    fullText.replaceAll("(", " ")
+    fullText.replaceAll(")", " ")
 
+    // split text into an array
+    let allWords = fullText.split(" ")
+    allWords = allWords.filter(word => isValidWord(word, commonWordList))
+    let foundWords = new Set(allWords)
+    // const words = fullText.split(" ")
+    // for (sequence of words) {
+    //     if (isValidWord(sequence))
+    //         foundWords.add(sequence)
+    // }
+
+    for (word of foundWords) {
+        const searchPattern = new RegExp(word, ig)
+        let count = (fullText.match(searchPattern) || []).length;
+        freqCounter.word = count
+    }
+    
+    // for each found word, get it's count and store it in an dictionary
     return freqCounter
 }
 

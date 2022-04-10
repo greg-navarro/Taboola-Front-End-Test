e546911df8af31b12337cf0470cd612086cde8dd
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2022-04-10 14:13:24 -0700

    leverages async/await to synchronously use the fetch api

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ getCommonWords(25);
 // Retrieves a list of the most commonly used words in the English language.
 // Words are retrieved from the wikipedia page at https://en.wikipedia.org/wiki/Most_common_words_in_English.
 // An array containing these words and several other common words are returned.
-function getCommonWords(numberWords) {
+async function getCommonWords(numberWords) {
     let commonWords = [];
-    let commonWordsPage;
-    // const requestOptions = {
-    //     method: 'GET',
-    //     mode: 'cors',
-    //     cache: 'no-cache',
-    // };
-    fetch('https://en.wikipedia.org/wiki/Most_common_words_in_English') //, requestOptions)
+    let commonWordsPage = fetch('https://en.wikipedia.org/wiki/Most_common_words_in_English') //, requestOptions)
         .then(response => response.text())
-        .then(rt => console.log(rt))
-    // console.log(commonWordsPage);
-}
+    console.log(commonWordsPage);
 
-// .body)
-// .then(rb => rb.text())
+    return commonWords;
+}

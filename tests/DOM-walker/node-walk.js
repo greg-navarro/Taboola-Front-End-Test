0abf28f79d05a058aaceee3940e946482ffc9f1d
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2022-04-11 09:31:05 -0700

    Update node-walk.js

--- tests/DOM-walker/node-walk.js
+++ tests/DOM-walker/node-walk.js
@@ -1,10 +1,11 @@
 // create a treewalker with the <body> element as the root node
 // do not filter at this point
 // prints out the node to the console
-const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_ALL, null);
+const walker = document.createTreeWalker(document.getElementById('content'), NodeFilter.SHOW_TEXT);
 let currentNode = walker.currentNode;
 
 while (currentNode) {
-    console.log(currentNode);
+    console.log(currentNode.nodeValue);
     currentNode = walker.nextNode();
 }
+

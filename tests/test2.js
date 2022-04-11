5b4eeb350071890be071e7af9303de121b80c9b1
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2022-04-11 14:33:57 -0700

    begins implementing procedure to update the text nodes of the dom, also adds some test code

--- tests/test2.js
+++ tests/test2.js
@@ -20,6 +20,14 @@ function isValidWord (wordIn, commonWordList) {
     return valid;
 }
 
+// TEST function TODO delete
+function isValidWord (word) { 
+    if (isNaN(word)) {
+        return true;
+    }
+    return false; 
+}
+
 // given a node, visits all descendent text nodes
 // and gathers an array of references to each
 function getTextNodes (rootNode) {
@@ -69,4 +77,34 @@ const wordFrequencies = mapWordFrequencies(
     );
 // convert wordFreqencies to a sorted array, with the most frequent words ordered before the least
 const sortedOccurrenceList = Object.entries(wordFrequencies).sort((a, b) => b[1] - a[1])
+// substitute those words that occur most often with the number of times they occur
+const maxSubstitutions = 25;
+let substitutions = 0;
+let i = 0;
+while (substitutions < maxSubstitutions && i < sortedOccurrenceList.length) {
+    const current = sortedOccurrenceList[i];
+    const currentWord = current[0];
+    console.log(current);
+    // only make substitutions if the word meets our requirements for validity
+    if (isValidWord(currentWord)) {
+        console.log("valid word")
+        let wordOccurs = current[1];
+        substitutions++;
+        // transverse textNodes from the relevant portion of the page
+        // replace each instance of the target word with the number of times it occurs
+        for (node of textNodes) {
+            const currentNodeValue = node.nodeValue;
+            // console.log(currentNodeValue);
+        //     if (typeof currentValue === "string") {
+        //         // console.log(currentValue);
+        //         const newValue = currentValue.replaceAll(currentWord, wordOccurs);
+        //         node.nodeValue = newValue;
+        //     } else { 
+        //         // console.log("Not a string"); 
+        //     }
+        }
+    }
+
+    i++;
+}
 

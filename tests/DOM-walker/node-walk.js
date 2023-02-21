ef7281041a05273170f716bc968b3c0f4375fa90
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2023-02-20 17:42:47 -0800

    asdf

--- tests/DOM-walker/node-walk.js
+++ tests/DOM-walker/node-walk.js
@@ -9,3 +9,11 @@ while (currentNode) {
     currentNode = walker.nextNode();
 }
 
+// example of how value will be parsed
+let test = "mw-parser-output .refbegin{font-size:90%;margin-bottom:0.5em}.mw-parser-output .refbegin-hanging-indents>ul{margin-left:0}.mw-parser-output .refbegin-hanging-indents>ul>li{margin-left:0;padding-left:3.2em;text-indent:-3.2em}.mw-parser-output .refbegin-hanging-indents ul,.mw-parser-output .refbegin-hanging-indents ul li{list-style:none}@media(max-width:720px){.mw-parser-output .refbegin-hanging-indents>ul>li{padding-left:1.6em;text-indent:-1.6em}}.mw-parser-output .refbegin-columns{margin-top:0.3em}.mw-parser-output .refbegin-columns ul{margin-top:0}.mw-parser-output .refbegin-columns li{page-break-inside:avoid;break-inside:avoid-column}"
+const whiteSpace = /(\n|\t|\v|\r|\f)/g
+test = test.replaceAll(whiteSpace, " ")
+console.log(test)
+const punctuation = /[?.,"'\[\]\(\)]/g 
+test = test.replaceAll(punctuation, "")
+console.log(test.split(" "))
